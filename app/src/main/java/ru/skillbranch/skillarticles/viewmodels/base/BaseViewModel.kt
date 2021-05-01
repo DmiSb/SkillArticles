6ecbe38b53680ff00acc9ba6d5057a2838f5ddaa
DmiSb <dvsbrodov@gmail.com>
2021-05-01 21:30:21 +0500

    Решение к уроку "Activity Result Callback Api" - part 5

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/base/BaseViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/base/BaseViewModel.kt
@@ -60,6 +60,7 @@ abstract class BaseViewModel<T : IViewModelState>(private val handleState: Saved
      * повторно
      */
     @UiThread
+    @VisibleForTesting(otherwise = VisibleForTesting.PROTECTED)
     fun notify(content: Notify) {
         notifications.value = Event(content)
     }
